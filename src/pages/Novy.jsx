671f7e7cdfe4cabd85cb876ada0c62be7e5633f3
krevetka159 <dita.chabicovska@gmail.com>
2023-11-24 19:44:12 +0100

    pages

---
+++ src/pages/Novy.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+
+
+const Novy = () => {
+    return(
+        <Box sx={{m:2, mt:8}}>
+            
+        </Box>
+    )
+}
+
+export default Novy
---
+++ src/pages/Prehled.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {
+  DataGridPremium,
+  useGridApiRef,
+  useKeepGroupedColumnsHidden,
+} from '@mui/x-data-grid-premium';
+import { useMovieData } from '@mui/x-data-grid-generator';
+import { Box } from '@mui/material';
+
+const Prehled = () => {
+    return(
+        <Box sx={{m:2, mt:8}}></Box>
+    )
+}
+
+export default Prehled
