e3d7399553a89fb73f013f774b9f990fdb1fd7ea
krevetka159 <dita.chabicovska@gmail.com>
2023-11-24 21:15:48 +0100

    add button

--- src/pages/Sablony.jsx
+++ src/pages/Sablony.jsx
@@ -1,17 +1,34 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import { GridRowModes,
+    DataGrid,
+    GridToolbarContainer,
+    GridActionsCellItem,
+    GridRowEditStopReasons, } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 import {
     randomCreatedDate,
     randomTraderName,
     randomUpdatedDate,
   } from '@mui/x-data-grid-generator';
 
-
+  function EditToolbar(props) {
+  
+    return (
+      <GridToolbarContainer>
+        <Button color="primary" startIcon={<AddIcon />}>
+          Nová šablona
+        </Button>
+      </GridToolbarContainer>
+    );
+  }
 const Sablony = () => {
     return(
         <Box sx={{m:2, mt:8}}>
-            <DataGrid editMode="row" rows={rows} columns={columns} />
+            <DataGrid editMode="row" rows={rows} columns={columns} slots={{
+          toolbar: EditToolbar,
+        }}/>
         </Box>
     )
 }
