fe316c8d3d4448cacc1d13881471a7e2431e3c31
krevetka159 <dita.chabicovska@gmail.com>
2023-11-24 19:45:05 +0100

    gridpro

---
+++ src/GridPro.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {
+  DataGridPremium,
+  useGridApiRef,
+  useKeepGroupedColumnsHidden,
+} from '@mui/x-data-grid-premium';
+import { useMovieData } from '@mui/x-data-grid-generator';
+import { Box } from '@mui/material';
+
+export default function RowGroupingBasicExample() {
+  const data = useMovieData();
+  const apiRef = useGridApiRef();
+
+  const initialState = useKeepGroupedColumnsHidden({
+    apiRef,
+    initialState: {
+      rowGrouping: {
+        model: [],
+      },
+    },
+  });
+
+  return (
+    <Box style={{ height: 400, width: '100%' }}>
+      <DataGridPremium {...data} apiRef={apiRef} initialState={initialState} />
+    </Box>
+  );
+}
