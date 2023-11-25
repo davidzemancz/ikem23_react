30a56cca0e230b4496f2382175ad2ed3df1176eb
krevetka159 <dita.chabicovska@gmail.com>
2023-11-25 15:02:39 +0100

    načtení z databáze

--- src/pages/Sablony.jsx
+++ src/pages/Sablony.jsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import { GridRowModes,
     DataGrid,
     GridToolbarContainer,
     GridActionsCellItem,
     GridRowEditStopReasons, } from '@mui/x-data-grid';
 import Button from '@mui/material/Button';
+import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
-import {
-    randomCreatedDate,
-    randomTraderName,
-    randomUpdatedDate,
-  } from '@mui/x-data-grid-generator';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 
   function EditToolbar(props) {
   
@@ -24,9 +23,45 @@ import {
     );
   }
 const Sablony = () => {
+  const [rows, setRows] = useState([])
+
+  const columns = [
+    { field: 'name', headerName: 'Název', width: 200 },
+    { field: 'description', headerName: 'Popis', width: 250},
+    {
+      field: " ",
+      sortable: false,
+      width: 80,
+      renderCell: (params) => {
+        return (
+          <IconButton
+            onClick={(e) => onButtonClick(e, params.row)}
+            variant="contained"
+          >
+            <EditIcon></EditIcon>
+          </IconButton>
+        );
+      }
+    }]
+
+  const onButtonClick = (e, row) => {
+    e.stopPropagation();
+    console.log(row);
+  };
+
+    useEffect (() =>{
+        axios.get('/ImportTemplate', { params: {} })
+        .then(res => {
+            setRows(res.data)
+        })
+        .catch(ex => {
+            console.log(ex)
+        })
+    },[])
+
     return(
         <Box sx={{m:2, mt:8}} style={{width:'100%', height:'100%'}}>
-          <Box style={{width:'100%', height:'100%'}}>
+          <Box style={{width:'100%', height:'80vh'}}>
             <DataGrid rows={rows} columns={columns} slots={{
           toolbar: EditToolbar,
         }}/>
@@ -38,8 +73,3 @@ const Sablony = () => {
 export default Sablony
 
 
-const rows = {}
-const columns = [
-    { field: 'id', headerName: 'Id', width: 180 },
-    { field: 'name', headerName: 'Název', width: 180 },
-    { field: 'description', headerName: 'Popis', width: 180},]
